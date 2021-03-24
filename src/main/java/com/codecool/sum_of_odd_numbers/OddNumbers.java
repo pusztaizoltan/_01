44bac5d2d67332d58144421a0899e2a32c05ce9c
pusztaizoltan <a.pusztaizoltan@gmail.com>
2021-03-24 15:08:48 +0100

    add implementation

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -5,6 +5,9 @@ import java.util.List;
 public class OddNumbers {
 
     public static Integer sum(List<Integer> numbers) {
-        return null;
+        return numbers.stream()
+                      .filter(number -> number%2 == 1 || number%2 == -1)
+                      .mapToInt(Integer::intValue)
+                      .sum();
     }
 }
